683d0e8dc3df38d53d9ffcf7bf48110dae52babf
dennis0324 <dennis0324@naver.com>
2022-07-20 21:54:57 +0900

    백엔드 초기화

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+console.log("testing")
+
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+app.get('/',(req,res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log("testing")
+})
